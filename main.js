e864ca028f3e12ebf258ea2cd6f58081ce366877
Marko Lovic <markolovic@hotmail.com>
2016-10-13 00:52:00 +0200

    Add error reporting to toggleMenu fn

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ var toggleMenu = function() {
   if ($("nav .container").css("right") == '0px') {
     $("nav .container").animate({'right': '-200px'});
   }
-  else {
+  else if ($("nav .container").css("right") == '-200px') {
     $("nav .container").animate({'right': '0px'});
+  }
+  else {
+    console.log("Error toggling menu!");
   }
 };
